283ef904214b0e8030199df6cab80f29197de838
Lorenzo <tunedconsulting@gmail.com>
2015-01-10 18:02:14 +0100

    changes at setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='PramanthaSinglePage',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='Lorenzo',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
