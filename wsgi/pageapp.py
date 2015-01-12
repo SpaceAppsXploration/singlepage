760b5f43c41377a3c290a3606922e4c536bd38bc
Lorenzo <tunedconsulting@gmail.com>
2015-01-12 02:44:48 +0100

    newsletter saving improvements

--- wsgi/pageapp.py
+++ wsgi/pageapp.py
@@ -26,9 +26,18 @@ def hello():
 @app.route("/subscribe", methods=['POST'])
 def subscribe():
     mail = request.form['email']
-    with open(dir+'/data/newsletter.save', 'a') as f:
+    data = open(dir+'/data/newsletter.save').read()
+    position = data.find(mail)
+    if position == -1:
+        with open(dir+'/data/newsletter.save', 'a') as f:
             f.write(mail+' \n')
-    return redirect(url_for('.hello', message='You subscribed the newsletter'))
+        return redirect(url_for('.hello', message=message_ok))
+
+    return redirect(url_for('.hello', message=message_already))
+
+
+message_ok = 'You subscribed the newsletter. Wait for confirmation email. Thanks for your interest!'
+message_already = 'You already subscribed with this email address. Wait for confirmation in the next 24 hours.'
 
 
 if __name__ == "__main__":
